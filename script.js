6cbe1172a055e8f7f4e590d44cea7c842248375d
melih0132 <melih.cetinkaya.32@gmail.com>
2024-10-29 20:06:09 +0100

    ptit belm

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const userLanguage = navigator.language || navigator.userLanguage;
 
 if (userLanguage.startsWith('fr')) {
   window.location.href = '/fr';
-} else if (userLanguage.startsWith('en')) {
-  window.location.href = '/en';
 } else {
-  window.location.href = '/fr';
+  window.location.href = '/en';
 }
